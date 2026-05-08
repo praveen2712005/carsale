96e5b2598dd9423da9a669bdc8d76e8d9a8c095f
praveen2712005 <praveen271jan@gmail.com>
2026-05-08 15:32:53 +0530

    backend final commit

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://carsale-backend-2.onrender.com'
+    baseURL: 'https://carsale-backend-3.onrender.com',
   
     
 });
